5daabaf8ec041436753f8e3702b5f09f37af29ec
Christopher Moffett <christophmoffett@gmail.com>
2017-09-27 23:14:49 -0700

    Final Assignments

--- Chris_Moffett_Week6_Part1.asm
+++ Chris_Moffett_Week6_Part1.asm
@@ -1,7 +1,7 @@
 ; Program Name:  Chris_Moffett_Week6_Part1
 ; Program Description: Reverse string with modules
 ; Author: Christopher Moffett
-; Date:   9/28/2016
+; Date:   10/1/2016
 
 .386
 .model flat,c		; C calling convention
@@ -15,23 +15,24 @@ includelib ucrt
 
 ; Declare the external procedures we will be calling
 extern ReverseString:PROC	; my modular proc
-
-STDIN EQU 0
-STDOUT EQU 1
+extern exit:PROC
 
 .data
-nl		dword 0ah		; new line character
 fwdString byte "A super calloused fragile mystic hexed by halitosis",0
 stringSize = ($ - fwdString) - 1
 rvdString byte ?
 
 .code
 main proc
-mov ecx, stringSize		;load loop count
-push OFFSET fwdString
-push OFFSET rvdString
-call ReverseString
-		
-main endp
 
+	mov  ecx, stringSize
+	push ecx				;push loop counter on stack so module can pop it (unnecessary argument?)
+	push OFFSET fwdString	;push offsets
+	push OFFSET rvdString
+
+	call ReverseString
+	add	 esp, 12			;inc stack pointer
+
+	call exit
+main endp
 end main
--- Chris_Moffett_Week6_Part1b.asm
+++ Chris_Moffett_Week6_Part1b.asm
@@ -1,32 +1,47 @@
 ; Program Name:  Chris_Moffett_Week6_Part1
 ; Program Description: This module contains reverse string procedure
 ; Author: Christopher Moffett
-; Date:   9/28/2016
+; Date:   10/1/2016
 
 .386
 .model flat,c
 
-.code
+aParam EQU [ebp+8]
+bParam EQU [ebp+12]
+cParam EQU [ebp+16]
 
+.code
 ;-----------------------------------------------------
 ; Description:
 ; Receives: String
 ; Returns: Reversed String
 ;-----------------------------------------------------
 ReverseString PROC
+
 	; Function prolog
-	push	ebp
-	mov		ebp, esp
+	push ebp
+	mov	ebp, esp
+
+	mov ecx, 0			;zero out loop
 
 	; Compute the result
-	mov		esi, 0
-	mov		eax, [ebp + 8]		; rvdString offset
-	mov		ebx, [ebp + 12]		; fwdString offset
+	mov	eax, aParam		; rvdString offset
+	mov	ebx, bParam		; fwdString offset
+	mov	ecx, cParam		; loop counter
 
+	L1: push [ebx]		;push value of offset fwdString ref onto stack
+	    inc ebx			;inc ref
+		loop L1
+
+	mov	ecx, [ebp + 16]	; loop counter
+
+	L2: pop ebx			;pop value onto ebx
+		mov [eax], bl	;move value into value of eax ref
+		inc eax			;inc eax ref
+		loop L2
 
 	; Function epilog
-	pop		ebp
-	ret		12		; Three 32-bit items on stack
+	pop	ebp
+	ret	
 ReverseString ENDP
-
 END
